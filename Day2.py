2c7e42d8b1d3fc3951cbe4abd15f179cfcfb600f
Zhiyuan Luo <49680424+Morgenlefay@users.noreply.github.com>
2019-09-25 19:29:58 +0800

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+marker = pd.read_excel('C:/Users/Administrator/Documents/Python/Marker.xlsx')
+print(marker.shape)
+print(marker.columns)
+print(marker.head())
+print('===============')
+print(marker.tail())
